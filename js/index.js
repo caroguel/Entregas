f5ed844be74dcffc344a414184fea59618fc02f6
caroguel <138170893+caroguel@users.noreply.github.com>
2023-11-17 22:10:11 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let esMayordeEdad = prompt("Eres una persona mayor de edad?");
+let esMayordeEdad = prompt("Eres una persona mayor de edad? Ingresa tu");
 
 if (esMayordeEdad >=18){
     console.log ("Puedes continuar");
@@ -15,34 +15,30 @@ if(SEXO === "femenino"){
 }else{
     console.log("El sexo ingresado no es valido.");
 }
-
 let peso = parseFloat(prompt("Ingresa tu peso en kilogramos"));
-
-if ( parseInt(peso)) {
-console.log("El peso ingresado es: " + peso);
-} else {
-console.log("La entrada no es un número válido");
-}
-
 let altura = parseFloat (prompt ("Ingresa tu altura en metros"));
 
-if (parseInt(altura)) {
-console.log("La altura ingresada es: " + altura);
-} else {
-console.log("La entrada no es un número válido");
+function calcularIMC () {
+    let continuar = true;
+
+    while (continuar){
+        let peso = parseFloat(prompt("Ingresa tu peso en kilogramos"));
+        let altura = parseFloat (prompt ("Ingresa tu altura en metros"));
+        let IMC = peso / (altura * altura);
+        if (IMC < 18.5) {
+            console.log(" Tu IMC es " + IMC + " Bajo peso");
+        } else if (IMC >= 18.5 && IMC <= 24.9) {
+            console.log(" Tu IMC es " + IMC + " Peso saludable");
+        } else if (IMC >= 25 && IMC <= 29.9) {
+            console.log(" Tu IMC es " + IMC + " Sobrepeso");
+        } else {
+            console.log(" Tu IMC es " + IMC + " Considera Obesidad");
+    }
+    let respuesta = prompt("¿Deseas calcular el IMC nuevamente? (SI/NO)").toUpperCase();
+        if (respuesta !== "SI") {
+            continuar = false;
+        }
+    }
 }
 
-// Calcular el IMC
-// IMC = peso / (altura * altura)
-
-const IMC = peso / (altura * altura);
-
-if (IMC < 18.5) {
-    console.log(IMC + " " + "Bajo peso");
-} else if (IMC >= 18.5 && IMC <= 24.9) {
-    console.log(IMC + " " + "Peso saludable");
-} else if (IMC >= 25 && IMC <= 29.9) {
-    console.log(IMC + " " + "Sobrepeso");
-} else {
-    console.log(IMC + " " + "Considera Obesidad");
-}
+calcularIMC();
